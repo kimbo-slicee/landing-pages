4bacdbca4fcf61538affc4b5b7eb7cc2d11aa126
mohammed el aouri <mohammedelaouri@gmail.com>
2025-12-11 15:45:53 +0100

    `Refactoring project and fix design bugs`

--- 03-Real-Estate-Web-App/utils/createElement.js
+++
@@ -1,6 +0,0 @@
-export default function createElement(tag, className = "", text = "") {
-    const el = document.createElement(tag);
-    if (className) el.className = className;
-    if (text) el.textContent = text;
-    return el;
-}
